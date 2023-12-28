4933a603afdf4daa1fab5887da6711946ad30809
christian.klinton <christian.klinton@wearesprout.se>
2023-12-28 21:56:35 +0100

    terser broken. update styled components

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 import ts from "rollup-plugin-ts";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 
 const packageJson = require("./package.json");
 
